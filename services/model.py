935a8299f13eada191e5f0be23eae55a15a32e7a
Vikas Palakurthi <vpalakur@iu.edu>
2016-09-21 12:35:03 -0400

    fixes #3, fixes#4

---
+++ services/model.py
@@ -0,0 +1,42 @@
+import MySQLdb as mysql
+
+db = mysql.connect(host="localhost", port=3306, user="vikas", passwd="passfordb", db="testdb")
+cursor = db.cursor()
+def insertUser(firstName, lastName, email, password, role="guest"):
+# Adds new user details to the table. By default, users are guests.
+	insertQuery = """
+	INSERT INTO users (FirstName, LastName, Email, Password, Role)
+	VALUES
+	({firstName}, {lastName}, {email}, {password}, {role})
+	;
+	""".format(firstName="'"+firstName+"'", lastName="'"+lastName+"'", email="'"+email+"'", password="'"+password+"'", role="'"+role+"'")
+	try:
+		cursor.execute(insertQuery)
+		db.commit()
+	except Exception as e:
+		print(e)
+		return False
+	else:
+		return True
+
+def findUser(email, password=""):
+# check if email is in the table.
+# if in the table, find the corresponding password
+	findQuery = "SELECT Password FROM testdb.users WHERE Email='"+email+"';"
+	try:
+		cursor.execute(findQuery)
+	except Exception as e:
+		print(e)
+		return False
+	else:
+		dbPwd = cursor.fetchone()
+		if not dbPwd:
+			#user non existent
+			return 99
+		else:
+			if password == dbPwd[0]:
+				# user exists and correct password
+				return 11
+			else:
+				# user exists, but password does not match
+				return 19
