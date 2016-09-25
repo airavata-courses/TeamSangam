2a2a3e575218da38abffe5890f8c45be575ad042
Rama Arvabhumi <ramarvab@MacBook-Pro.local>
2016-09-24 22:39:24 -0400

    Data Ingestor Final Code

---
+++ SGA_REST_DataIngest/src/main/java/edu/sga/sangam/db/DBConnections.java
@@ -0,0 +1,38 @@
+package edu.sga.sangam.db;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.mongodb.MongoClient;
+
+public class DBConnections {
+	
+	private static DBConnections instance = null;
+	private static MongoClient mongo = null;
+	
+	private DBConnections()
+	{
+		try{
+		    Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+			mongoLogger.setLevel(Level.SEVERE); 
+			mongo = new MongoClient("localhost",27017);
+			
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+	static{
+		instance = new DBConnections();
+	}
+	
+	public static DBConnections getInstance()
+	{
+		return instance;
+	}
+	public MongoClient getConnection(){
+		return mongo;
+	}
+}
