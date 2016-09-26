f09897088c75cb71b30d095506f8815d2a137255
Pradeep Kumar Ravilla <pravilla@silo.soic.indiana.edu>
2016-09-26 16:24:49 -0400

    updated the paths of the script files

--- CodeDeployScripts/beforeInstall.sh
+++ CodeDeployScripts/beforeInstall.sh
@@ -13,13 +13,16 @@ sudo pip install virtualenv
 
 
 echo 'Creating python 3.4 virtual environment'
-cd /home/ec2-user/SGA_Sangam_WeatherApp/SGA_REST_login/services
+#cd /home/ec2-user/SGA_Sangam_WeatherApp/SGA_REST_login/services
+cd /home/ec2-user/SGA_REST_login/services
+
 sudo /usr/local/bin/virtualenv -p /usr/bin/python3.4 weatherApp
 sudo alternatives --set python /usr/bin/python2.7
 sudo source weatherApp/bin/activate
 
 echo 'Installing required python modules..'
-sudo pip install -r /home/ec2-user/SGA_Sangam_WeatherApp/requirements.txt 
+#sudo pip install -r /home/ec2-user/SGA_Sangam_WeatherApp/requirements.txt 
+sudo pip install -r /home/ec2-user/requirements.txt
 
 echo 'check if maven is installed'
 mvn --version
--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -1,8 +1,7 @@
 echo 'Installing Sangam Weather App to Maven'
 
-cd '/home/ec2-user/SGA_Sangam_WeatherApp/SGA_REST_DataIngest'
+cd '/home/ec2-user/SGA_REST_DataIngest'
 mvn -e clean install
 
-cd '/home/ec2-user/SGA_Sangam_WeatherApp/SGA_REST_login'
-cp ./* $CATALINA_HOME/webapps/
-
+cd '/home/ec2-user/SGA_REST_login'
+cp -r ./* $CATALINA_HOME/webapps/ROOT
