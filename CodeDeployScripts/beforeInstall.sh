1b50e0a629cea02cbce67ef6bf1a85fa36354c17
Pradeep Kumar Ravilla <pravilla@silo.soic.indiana.edu>
2016-09-26 12:19:44 -0400

    deploy bug fix

--- CodeDeployScripts/beforeInstall.sh
+++ CodeDeployScripts/beforeInstall.sh
@@ -13,13 +13,13 @@ sudo pip install virtualenv
 
 
 echo 'Creating python 3.4 virtual environment'
-cd /home/ec2-user/sangamWeatherApp/SGA_REST_login/services
+cd /home/ec2-user/SGA_Sangam_WeatherApp/SGA_REST_login/services
 sudo /usr/local/bin/virtualenv -p /usr/bin/python3.4 weatherApp
 sudo alternatives --set python /usr/bin/python2.7
 sudo source weatherApp/bin/activate
 
 echo 'Installing required python modules..'
-sudo pip install -r /home/ec2-user/sangamWeatherApp/requirements.txt 
+sudo pip install -r /home/ec2-user/SGA_Sangam_WeatherApp/requirements.txt 
 
 echo 'check if maven is installed'
 mvn --version
--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -1,8 +1,8 @@
 echo 'Installing Sangam Weather App to Maven'
 
-cd '/home/ec2-user/sangamWeatherApp/SGA_REST_DataIngest'
+cd '/home/ec2-user/SGA_Sangam_WeatherApp/SGA_REST_DataIngest'
 mvn -e clean install
 
-cd '/home/ec2-user/sangamWeatherApp/SGA_REST_login'
+cd '/home/ec2-user/SGA_Sangam_WeatherApp/SGA_REST_login'
 cp ./* $CATALINA_HOME/webapps/
 
