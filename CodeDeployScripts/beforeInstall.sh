78d2a81c7baab738313e220c17142ebe3c6112da
Pradeep Kumar Ravilla <pravilla@silo.soic.indiana.edu>
2016-09-26 20:03:47 -0400

    fixed install scripts for deploy

--- CodeDeployScripts/beforeInstall.sh
+++ CodeDeployScripts/beforeInstall.sh
@@ -1,15 +1,15 @@
 ps -ef | grep tomcat | awk '{print $2}' | xargs kill -9
 
 echo 'Installing python 3.4...'
-sudo yum install python34 || true
+sudo yum -y install python34
 
 echo 'Installing some dependencies for setting up python 3.4 environment...'
-sudo yum install python34-pip || true
-sudo yum install python-devel mysql-devel || true
-sudo yum install python34-devel || true
-sudo yum install gcc || true
-sudo alternatives --set python /usr/bin/python3.4 || true
-sudo pip install virtualenv || true
+sudo yum -y install python34-pip
+sudo yum -y install python-devel mysql-devel
+sudo yum -y install python34-devel
+sudo yum -y install gcc
+sudo alternatives --set python /usr/bin/python3.4
+sudo pip install virtualenv
 
 
 echo 'Creating python 3.4 virtual environment'
