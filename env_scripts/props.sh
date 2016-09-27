b013a8e5a74549d1205baa73c9546126ec9b07bc
Vikas Palakurthi <vpalakur@iu.edu>
2016-09-27 13:48:58 -0400

    Changes in props.sh

--- env_scripts/props.sh
+++ env_scripts/props.sh
@@ -1,2 +1,2 @@
-echo $MYSQL_USER > ../SGA_REST_login/services/mysql.properties
-echo $MYSQL_PWD >> ../SGA_REST_login/services/mysql.properties
+echo 'MYSQL_USER='$MYSQL_USER > ../SGA_REST_login/services/mysql.properties
+echo 'MYSQL_PWD='$MYSQL_PWD >> ../SGA_REST_login/services/mysql.properties
