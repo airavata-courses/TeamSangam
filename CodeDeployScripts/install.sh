4c68bb8c4acadbd3d2c44ea6dc66c669e2cce7db
Vikas Palakurthi <vpalakur@iu.edu>
2016-09-27 19:05:16 -0400

    Added commands in install script to copy the homepage code to Tomcat

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -5,3 +5,5 @@ mvn -e clean install
 
 cd '/home/ec2-user/SGA_REST_login'
 cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
+cd '/home/ec2-user/SGA_REST_Homepage'
+cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
