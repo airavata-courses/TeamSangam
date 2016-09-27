e3da4b593b954893026de1426e0529c56f0c2c1e
Pradeep Kumar Ravilla <pravilla@silo.soic.indiana.edu>
2016-09-26 22:33:30 -0400

    deploy bug fixes

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -4,4 +4,4 @@ cd '/home/ec2-user/SGA_REST_DataIngest'
 mvn -e clean install
 
 cd '/home/ec2-user/SGA_REST_login'
-cp -r ./* $CATALINA_HOME/webapps/ROOT
+cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
