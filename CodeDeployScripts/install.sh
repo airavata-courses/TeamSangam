cef1faafbd1c8326487a1a13e499f6abb4bb55bf
praddyinstant <pradeep421@gmail.com>
2016-09-28 15:33:30 -0400

    updated the dataingest endpoint

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -3,6 +3,9 @@ echo 'Installing Sangam Weather App to Maven'
 cd '/home/ec2-user/SGA_REST_DataIngest'
 mvn -e clean install
 
+echo 'Deploying the war file in Tomcat'
+sudo cp ./target/SGA_REST_DataIngest.war /opt/apache-tomcat-8.0.37/webapps/
+
 cd '/home/ec2-user/SGA_REST_login'
 cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
 cd '/home/ec2-user/SGA_REST_Homepage'
@@ -14,6 +17,3 @@ sudo cp ./scripts/main.js /opt/apache-tomcat-8.0.37/webapps/ROOT/scripts/
 sudo cp ./services/cors.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
 sudo cp ./services/homepage.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
 sudo cp ./stylesheets/bootstrap.min.css /opt/apache-tomcat-8.0.37/webapps/ROOT/stylesheets/
-
-
-
