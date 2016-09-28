e0d37be52aa346eea016228f1ea6b2dcf73dec11
praddyinstant <pradeep421@gmail.com>
2016-09-28 01:48:41 -0400

    deploy fixes

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -8,12 +8,12 @@ cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
 cd '/home/ec2-user/SGA_REST_Homepage'
 #cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
 
+
 sudo cp index.html /opt/apache-tomcat-8.0.37/webapps/ROOT/
 sudo cp ./scripts/angular.min.js /opt/apache-tomcat-8.0.37/webapps/ROOT/scripts/
 sudo cp ./scripts/main.js /opt/apache-tomcat-8.0.37/webapps/ROOT/scripts/
 sudo cp ./services/cors.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
 sudo cp ./services/homepage.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
+sudo cp ./services/aws_key.properties /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
 sudo cp ./stylesheets/bootstrap.min.css /opt/apache-tomcat-8.0.37/webapps/ROOT/stylesheets/
-
-
 
