a9ef1cb7d5c4d5e579a0699d2ec8e0a3079feef5
praddyinstant <pradeep421@gmail.com>
2016-09-28 17:24:54 -0400

    resolving service endpoint issues

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -4,7 +4,7 @@ cd '/home/ec2-user/SGA_REST_DataIngest'
 mvn -e clean install
 
 echo 'Deploying the war file in Tomcat'
-sudo cp ./target/SGA_REST_DataIngest.war /opt/apache-tomcat-8.0.37/webapps/
+sudo mv ./target/SGA_REST_DataIngest.war /opt/apache-tomcat-8.0.37/webapps/sga/dataingestor.war
 
 cd '/home/ec2-user/SGA_REST_login'
 cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
