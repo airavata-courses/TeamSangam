a70bb30b559b5d8e372024e4f7bd8dd7c4f9782e
praddyinstant <pradeep421@gmail.com>
2016-10-18 19:34:41 -0400

    deployment changes fixes #48

--- CodeDeployScripts/beforeInstall.sh
+++ CodeDeployScripts/beforeInstall.sh
@@ -16,6 +16,23 @@ sudo alternatives --set python /usr/bin/python2.7
 echo 'Installing MySQL...'
 sudo yum –y install mysql55-server
 
+sudo service mysqld start
+
+sudo mysqladmin -u root password root
+
+sudo mysql -u root -proot << EOF
+create database weatherApp;
+use weatherApp;
+CREATE TABLE weatherApp.users (
+Id INT NOT NULL AUTO_INCREMENT,
+FirstName VARCHAR(45) NOT NULL,
+LastName VARCHAR(45) NOT NULL,
+Email VARCHAR(255) NOT NULL,
+Password VARCHAR(255) NOT NULL,
+Role VARCHAR(10) NOT NULL,
+PRIMARY KEY (Id),
+UNIQUE INDEX Id_UNIQUE (Id ASC));
+EOF
 
 echo 'check if maven is installed'
 mvn --version
--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -4,44 +4,44 @@ cd '/home/ec2-user/SGA_REST_DataIngest'
 mvn -e clean install
 
 echo 'Deploying the war file in Tomcat'
-sudo cp ./target/SGA_REST_DataIngest.war /opt/apache-tomcat-8.0.37/webapps/
+sudo mv ./target/SGA_REST_DataIngest.war /opt/apache-tomcat-8.0.37/webapps/
 
 echo 'Deploying StormClustering WebService'
 cd '/home/ec2-user/SGA_Rest_StormClustering'
 mvn install
-sudo cp ./target/SGA_Rest_StormClustering.war /opt/apache-tomcat-8.0.37/webapps/
+sudo mv ./target/SGA_Rest_StormClustering.war /opt/apache-tomcat-8.0.37/webapps/
 
 echo 'Deploying StormForecast WebService'
 cd '/home/ec2-user/SGA_REST_ForecastDecision'
 mvn install
-sudo cp ./target/SGA_REST_ForecastDecision.war /opt/apache-tomcat-8.0.37/webapps/
+sudo mv ./target/SGA_REST_ForecastDecision.war /opt/apache-tomcat-8.0.37/webapps/
 
 echo 'Deploying RunForecast WebService'
-cd '/home/ec2-user/SGA_Rest_RUNFORECAST'
+cd '/home/ec2-user/SGA_REST_Forecast'
 mvn install
-sudo cp ./target/SGA_Rest_RUNFORECAST.war /opt/apache-tomcat-8.0.37/webapps/
+sudo mv ./target/SGA_REST_Forecast.war /opt/apache-tomcat-8.0.37/webapps/
 
 echo 'Deploying StormDetection WebService'
 cd '/home/ec2-user/SGA_Rest_StormDetection'
 mvn install
-sudo cp ./target/SGA_Rest_StormDetection.war /opt/apache-tomcat-8.0.37/webapps/
+sudo mv ./target/SGA_Rest_StormDetection.war /opt/apache-tomcat-8.0.37/webapps/
 
 echo 'Deploying WeatherForecast client webservice'
 cd '/home/ec2-user/SGA_REST_WeatherForecastClient'
 mvn install
-sudo cp ./target/SGA_REST_WeatherForecastClient.war /opt/apache-tomcat-8.0.37/webapps/
+sudo mv ./target/SGA_REST_WeatherForecastClient.war /opt/apache-tomcat-8.0.37/webapps/
 
 cd '/home/ec2-user/SGA_REST_login'
-cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
+mv -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
 cd '/home/ec2-user/SGA_REST_Homepage'
 #cp -r ./* /opt/apache-tomcat-8.0.37/webapps/ROOT/
 
 
-sudo cp index.html /opt/apache-tomcat-8.0.37/webapps/ROOT/
-sudo cp ./scripts/angular.min.js /opt/apache-tomcat-8.0.37/webapps/ROOT/scripts/
-sudo cp ./scripts/main.js /opt/apache-tomcat-8.0.37/webapps/ROOT/scripts/
-sudo cp ./services/cors.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
-sudo cp ./services/homepage.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
-sudo cp ./services/aws_key.properties /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
-sudo cp ./stylesheets/bootstrap.min.css /opt/apache-tomcat-8.0.37/webapps/ROOT/stylesheets/
+sudo mv index.html /opt/apache-tomcat-8.0.37/webapps/ROOT/
+sudo mv ./scripts/angular.min.js /opt/apache-tomcat-8.0.37/webapps/ROOT/scripts/
+sudo mv ./scripts/main.js /opt/apache-tomcat-8.0.37/webapps/ROOT/scripts/
+sudo mv ./services/cors.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
+sudo mv ./services/homepage.py /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
+sudo mv ./services/aws_key.properties /opt/apache-tomcat-8.0.37/webapps/ROOT/services/
+sudo mv ./stylesheets/bootstrap.min.css /opt/apache-tomcat-8.0.37/webapps/ROOT/stylesheets/
 
