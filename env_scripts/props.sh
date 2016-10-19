2bc6c1dca8c67d2fd6de47516a2cbc511a962dfa
Vikas Palakurthi <vpalakur@iu.edu>
2016-10-18 21:06:44 -0400

    Added new instance in yml and script for adding oauth properties file.

--- env_scripts/props.sh
+++ env_scripts/props.sh
@@ -4,4 +4,6 @@ echo 'MYSQL_PWD='$MYSQL_PWD >> ../SGA_REST_login/services/mysql.properties
 echo 'API_KEY='$API_KEY > ../SGA_REST_Homepage/services/api_key.properties
 echo 'AWS_ACCESS_KEY='$AWS_ACCESS_KEY > ../SGA_REST_Homepage/services/aws_key.properties
 echo 'AWS_SECRET_KEY='$AWS_SECRET_KEY >> ../SGA_REST_Homepage/services/aws_key.properties
-
+echo 'GOAUTH_CLIENT_ID='$GOAUTH_CLIENT_ID > ../SGA_REST_login/services/googleOAuth.properties
+echo 'GOAUTH_CLIENT_SECRET='$GOAUTH_CLIENT_SECRET >> ../SGA_REST_login/services/googleOAuth.properties
+echo 'GOAUTH_APP_NAME='$GOAUTH_APP_NAME >> ../SGA_REST_login/services/googleOAuth.properties
