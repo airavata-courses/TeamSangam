c3df2893fe17555394321da14be20b522de1bd74
praddyinstant <pradeep421@gmail.com>
2016-10-21 00:28:50 -0400

    included docker installation script

--- CodeDeployScripts/beforeInstall.sh
+++ CodeDeployScripts/beforeInstall.sh
@@ -7,3 +7,12 @@ if [ "$?" -ne 0 ]; then
     sudo yum install -y apache-maven
     mvn --version
 fi
+
+docker -v
+if [ "$?" -ne 0 ]; then
+    echo 'Installing Docker...'
+    sudo yum update -y
+    sudo yum install -y docker
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+fi
