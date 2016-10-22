daa49e75db64af34585396c0e36c00887029f2b3
praddyinstant <pradeep421@gmail.com>
2016-10-22 00:47:06 -0400

    removed the unwanted links with the mongo container

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -48,18 +48,18 @@ docker run -d --name mongo -p 27017:27017 imongo
 
 docker run -d --name usermysqldb -p 3306:3306 iusermysqldb
 
-docker run -d --link usermysqldb:usermysqldb --name sgalogin -p 5000:5000 isgalogin
-
 docker run -d --name sgahome -p 5001:5001 isgahome
 
-docker run -d --link mongo:mongo --name sgagateway -p 8080:8080 isgagateway
+docker run -d --name sgagateway -p 8080:8080 isgagateway
 
 docker run -d --link mongo:mongo --name sgadataingest -p 8081:8081 isgadataingest
 
-docker run -d --link mongo:mongo --name sgastormdetection -p 8082:8082 isgastormdetection
+docker run -d --name sgastormdetection -p 8082:8082 isgastormdetection
 
-docker run -d --link mongo:mongo --name sgastormclustering -p 8083:8083 isgastormclustering
+docker run -d --name sgastormclustering -p 8083:8083 isgastormclustering
 
-docker run -d --link mongo:mongo --name sgaforecast -p 8084:8084 isgaforecast
+docker run -d  --name sgaforecast -p 8084:8084 isgaforecast
 
 docker run -d --link mongo:mongo --name sgaregistry -p 8085:8085 isgaregistry
+
+docker run -d --link usermysqldb:usermysqldb --name sgalogin -p 5000:5000 isgalogin
