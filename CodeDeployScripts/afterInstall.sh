7c72680b9b59ea12f41577cefbeb93f1119d7ce3
praddyinstant <pradeep421@gmail.com>
2016-10-22 01:36:43 -0400

    docker deploy bug fix

--- CodeDeployScripts/afterInstall.sh
+++ CodeDeployScripts/afterInstall.sh
@@ -18,7 +18,7 @@
 #cd /home/ec2-user/
 
 echo "Building MongoDB docker image"
-cd /home/ec2-user/DB_Dockers/registry_MongodDB
+cd /home/ec2-user/DB_Dockers/registry_MongodDB/
 docker build -t imongo .
 
 echo "Building mysqlDB docker image"
@@ -26,11 +26,11 @@ cd /home/ec2-user/DB_Dockers/userMySQLDB/
 docker build -t iusermysqldb .
 
 echo "Building login docker image"
-cd /home/ec2-user/SGA_REST_login
+cd /home/ec2-user/SGA_REST_login/
 docker build -t isgalogin .
 
 echo "Building Homepage docker image"
-cd /home/ec2-user/SGA_REST_Homepage
+cd /home/ec2-user/SGA_REST_Homepage/
 docker build -t isgahome .
 
 echo "Build maven package for WeatherForecastClient"
@@ -49,39 +49,39 @@ echo "Building gateway docker image"
 docker build -t isgagateway .
 
 echo "Build maven package for WeatherForecastClient"
-cd /home/ec2-user/SGA_REST_DataIngest
+cd /home/ec2-user/SGA_REST_DataIngest/
 mvn package
 echo "Building dataingest docker image"
 docker build -t isgadataingest .
 
 echo "Build maven package for StormDetection"
-cd /home/ec2-user/SGA_Rest_StormDetection
+cd /home/ec2-user/SGA_Rest_StormDetection/
 mvn package
 echo "Building stormdetection docker image"
 docker build -t isgastormdetection .
 
 echo "Building maven package for StormClustering"
-cd /home/ec2-user/SGA_Rest_StormClustering
+cd /home/ec2-user/SGA_Rest_StormClustering/
 mvn package
 echo "Building stormclustering docker image"
 docker build -t isgastormclustering .
 
 echo "Building maven package for Forecast"
-cd /home/ec2-user/SGA_REST_Forecast
+cd /home/ec2-user/SGA_REST_Forecast/
 mvn package
 
 echo "Building maven package for ForecastDecision"
-cd /home/ec2-user/SGA_REST_ForecastDecision
+cd /home/ec2-user/SGA_REST_ForecastDecision/
 mvn package
 mv ./target/SGA_REST_ForecastDecision.war /home/ec2-user/SGA_REST_ForecastDecision/SGA_REST_ForecastDecision.war
 
 echo "Building docker image for Forecast Decision & Run Forecast"
-cd /home/ec2-user/SGA_REST_Forecast
+cd /home/ec2-user/SGA_REST_Forecast/
 docker build -t isgaforecast .
 
 
 echo "Building maven package for Registry"
-cd /home/ec2-user/SGA_REST_Registry
+cd /home/ec2-user/SGA_REST_Registry/
 mvn package
 
 echo "Building docker image for Registry"
