da85653d1fb8fc7cfa67638ca2a6fb78ecb9dba1
praddyinstant <pradeep421@gmail.com>
2016-11-11 00:09:31 -0500

    changes for the home page deployment #70

--- CodeDeployScripts/kill_container.sh
+++ CodeDeployScripts/kill_container.sh
@@ -1,31 +1,17 @@
 set -e
 
-#docker rm -f mongo || true
+docker stop sgalogin || true
 
-#docker rm -f usermysqldb || true
+docker rm -f usermysqldb || true
 
-#docker rm -f sgalogin || true
+docker rm -f sgalogin || true
 
-#docker rm -f sgahome || true
-
-#docker rm -f sgagateway || true
-
-#docker rm -f sgadataingest || true
-
-#docker rm -f sgastormdetection || true
-
-#docker rm -f sgastormclustering || true
-
-#docker rm -f sgaforecast || true
-
-#docker rm -f sgaregistry || true
-
-#docker rmi imongo || true
+docker rmi isgalogin || true
 
 #docker rmi iusermysqldb || true
 
-docker stop $(docker ps -a -q) || true
+#docker stop $(docker ps -a -q) || true
 
-docker rm $(docker ps -a -q) || true
+#docker rm $(docker ps -a -q) || true
 
-docker rmi $(docker images -q) 
+#docker rmi $(docker images -q) 
