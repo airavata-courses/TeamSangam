e489c2fcf997ecb8b0b269b60bca93ae0406f98f
Vikas Palakurthi <vikaspro90@gmail.com>
2016-11-17 15:34:56 -0500

    [#79]deployment test

--- CodeDeployScripts/afterInstall.sh
+++ CodeDeployScripts/afterInstall.sh
@@ -28,15 +28,16 @@
 #echo "Building login docker image"
 #cd /home/ec2-user/SGA_REST_login/
 #docker build -t isgalogin .
-
+echo "Building Homepage docker image."
 cd /home/ec2-user/SGA_REST_Homepage
-if [[ $(docker images isgahome -q) ]]; then
-	echo "Deleting the old isgahome image."
-	docker rmi -f isgahome || true
-else	
-	echo "Building new Homepage docker image"
-	docker build -t isgahome .
-fi
+docker build -t isgahome .
+#if [[ $(docker images isgahome) ]]; then
+#	echo "Deleting the old isgahome image."
+#	docker rmi -f isgahome || true
+#else	
+#	echo "Building new Homepage docker image"
+#	docker build -t isgahome .
+#fi
 
 #echo "Build maven package for WeatherForecastClient"
 #cd /home/ec2-user/SGA_REST_WeatherForecastClient
