4557b93311ce61676b313e5df46e1af07cb03358
Vikas Palakurthi <vikaspro90@gmail.com>
2016-11-17 01:21:53 -0500

    [#70]deployment test

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -1,5 +1,21 @@
 set -e
 
-docker run -d --name usermysqldb -p 3306:3306 iusermysqldb
+#docker run -d --name mongo -p 27017:27017 imongo
 
-docker run -d --link usermysqldb:usermysqldb --name sgalogin -p 5000:5000 isgalogin
+#docker run -d --name usermysqldb -p 3306:3306 iusermysqldb
+
+docker run -d --name sgahome -p 5001:5001 isgahome
+
+#docker run -d --name sgagateway -p 8080:8080 isgagateway
+
+#docker run -d --link mongo:mongo --name sgadataingest -p 8081:8080 isgadataingest
+
+#docker run -d --name sgastormdetection -p 8082:8080 isgastormdetection
+
+#docker run -d --name sgastormclustering -p 8083:8080 isgastormclustering
+
+#docker run -d --name sgaforecast -p 8084:8080 isgaforecast
+
+#docker run -d --link mongo:mongo --name sgaregistry -p 8085:8080 isgaregistry
+
+#docker run -d --link usermysqldb:usermysqldb --name sgalogin -p 5000:5000 isgalogin
--- CodeDeployScripts/kill_container.sh
+++ CodeDeployScripts/kill_container.sh
@@ -1,17 +1,31 @@
 set -e
 
-docker stop sgalogin || true
+#docker rm -f mongo || true
 
-docker rm -f usermysqldb || true
+#docker rm -f usermysqldb || true
 
-docker rm -f sgalogin || true
+#docker rm -f sgalogin || true
 
-docker rmi isgalogin || true
+#docker rm -f sgahome || true
+
+#docker rm -f sgagateway || true
+
+#docker rm -f sgadataingest || true
+
+#docker rm -f sgastormdetection || true
+
+#docker rm -f sgastormclustering || true
+
+#docker rm -f sgaforecast || true
+
+#docker rm -f sgaregistry || true
+
+#docker rmi imongo || true
 
 #docker rmi iusermysqldb || true
 
-#docker stop $(docker ps -a -q) || true
+docker stop $(docker ps -a -q) || true
 
-#docker rm $(docker ps -a -q) || true
+docker rm $(docker ps -a -q) || true
 
-#docker rmi $(docker images -q) 
+docker rmi $(docker images -q) 
