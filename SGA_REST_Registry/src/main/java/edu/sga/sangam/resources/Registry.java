2e6d60b50fae24a05c2a537364a743ca12ec0384
ramarvab <ramarvab@gmail.com>
2016-11-17 21:19:52 -0500

    registry zookeeper service updated[#74]

--- SGA_REST_Registry/src/main/java/edu/sga/sangam/resources/Registry.java
+++ SGA_REST_Registry/src/main/java/edu/sga/sangam/resources/Registry.java
@@ -10,11 +10,31 @@ import javax.ws.rs.core.Response;
 
 import edu.sga.sangam.db.DBOperations;
 
-@Path("/resgitry")
+@Path("/registry")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class Registry {
 	
+	static private int portNumber;
+	static private String ipaddress;
+	private static final String endpointURI = "SGA_REST_Registry/sga/registry";
+	private static String serviceName =null;
+	private static String hostIP =null;
+	public static void main(String[] args) throws Exception {
+		if (args.length != 3) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+		serviceName = args[0];
+		ipaddress = args[1];
+		portNumber = Integer.parseInt(args[2]);
+		String url =String.format( "http://%s:%d/%s",
+				ipaddress,
+				portNumber,
+				endpointURI);
+		ZooKeeperService services = new ZooKeeperService();
+		services.registerService(serviceName,url);
+		
+	}
 	@Path("/dilogdata")
 	@POST
 	public Response dataIngestorRequest(DataIngestRequest input)
