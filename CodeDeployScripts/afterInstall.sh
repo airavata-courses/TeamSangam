2b74228b7f94a7b1f05478f251180f8630c97cef
Raja Simha <rajasimha1991@gmail.com>
2016-12-04 01:44:41 -0500

    [#72] updated hostnames in props files

--- CodeDeployScripts/afterInstall.sh
+++ CodeDeployScripts/afterInstall.sh
@@ -6,13 +6,13 @@ mv /home/ec2-user/forecast/SGA_REST_Forecast /home/ec2-user/
 echo "Moving the ForecastDecision files to /home/ec2-user"
 mv /home/ec2-user/forecast/SGA_REST_ForecastDecision /home/ec2-user/
 
-DIRECTORY=DB_Dockers
-cd /home/ec2-user
-if [ -d "$DIRECTORY" ]; then
-	echo "Directory already exists"
-else
-	mv /home/ec2-user/forecast/DB_Dockers /home/ec2-user/
-fi
+#DIRECTORY=DB_Dockers
+#cd /home/ec2-user
+#if [ -d "$DIRECTORY" ]; then
+	#echo "Directory already exists"
+#else
+	#mv /home/ec2-user/forecast/DB_Dockers /home/ec2-user/
+#fi
 	
 #echo "Building MongoDB docker image"
 #cd /home/ec2-user/DB_Dockers/registry_MongoDB/
@@ -25,11 +25,25 @@ fi
 
 echo "Building maven package for Forecast"
 cd /home/ec2-user/SGA_REST_Forecast/
-mvn package
+declare -a worker
+declare -i index
+index=0
+while IFS=':' read -r server privateip publicip
+do
+	if [[ "$server" == "SERVER"* ]]; then
+		worker[$index]=$publicip
+		index=$index+1
+		echo "$index"
+	fi
+done < /home/ec2-user/SGA_Rest_Forecast/system.properties
+echo ${worker[1]}
+echo ${worker[0]}
+echo ${worker[2]}
+mvn package -Dmaven.test.skip=true
 
 echo "Building maven package for ForecastDecision"
 cd /home/ec2-user/SGA_REST_ForecastDecision/
-mvn package
+mvn package -Dmaven.test.skip=true
 mv ./target/SGA_REST_ForecastDecision.war /home/ec2-user/SGA_REST_Forecast/target/SGA_REST_ForecastDecision.war
 
 echo "Building docker image for Forecast Decision & Run Forecast"
--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -15,24 +15,24 @@ fi
 echo "Starting new docker container sgaforecast"
 docker run -d --name sgaforecast -p 8084:8080 isgaforecast
 
-localip=$(ip addr show eth0 | awk '/inet /{split($2,a,"/");print a[1]}')
+#localip=$(ip addr show eth0 | awk '/inet /{split($2,a,"/");print a[1]}')
 
-while IFS=':' read -r server privateip publicip
-do
-   if [ "$privateip" == "$localip" ]; then
-        systemIP=$publicip
-        serverID=$server
-   fi
-done < /home/ec2-user/SGA_REST_Forecast/system.properties
+#while IFS=':' read -r server privateip publicip
+#do
+   #if [ "$privateip" == "$localip" ]; then
+        #systemIP=$publicip
+        #serverID=$server
+   #fi
+#done < /home/ec2-user/SGA_REST_Forecast/system.properties
 
 echo "Changing the directory to SGA_REST_Forecast"
 cd /home/ec2-user/SGA_REST_Forecast
 
-mvn exec:java -Dexec.mainClass=edu.sga.sangam.resources.RunForecast -Dexec.args="$serverID $systemIP 8084"
+nohup mvn exec:java -Dexec.mainClass=edu.sga.sangam.resources.RunForecastMain > /home/ec2-user/maven.out 2>&1 &
 
 echo "Changing the directory to SGA_REST_ForecastDecision"
 cd /home/ec2-user/SGA_REST_ForecastDecision
 
-mvn exec:java -Dexec.mainClass=edu.sga.sangam.resources.ForecastDecision -Dexec.args="$serverID $systemIP 8084"
+nohup mvn exec:java -Dexec.mainClass=edu.sga.sangam.resources.ForecastDecisionMain > /home/ec2-user/maven.out 2>&1 &
 
 docker rmi $(docker images -f "dangling=true" -q) || true
