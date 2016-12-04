eadb070700effd4f87a2fe6724afd821858f3602
Raja Simha <rajasimha1991@gmail.com>
2016-12-04 16:29:29 -0500

    [72] updated db constants

--- SGA_REST_Registry/src/main/java/edu/sga/sangam/db/DBConstants.java
+++ SGA_REST_Registry/src/main/java/edu/sga/sangam/db/DBConstants.java
@@ -2,9 +2,9 @@ package edu.sga.sangam.db;
 
 public class DBConstants {
 	static public final String DB_HOST ="localhost";
-	static public final String gateway = "54.183.132.116"
-	static public final String worker1 = "54.193.9.114"
-	static public final String worker2 = "52.53.192.165"
+	static public final String gateway = "54.183.132.116";
+	static public final String worker1 = "54.193.9.114";
+	static public final String worker2 = "52.53.192.165";
 	static public final int DB_Port =27017;
 	static public final String DB_Name ="db_SGA";
 	static public final String DB_Collection_Log ="registry_collection";
