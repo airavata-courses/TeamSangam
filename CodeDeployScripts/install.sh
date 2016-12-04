291a74ad1c6bfa3dad2441726334675351a3f158
praddyinstant <pradeep421@gmail.com>
2016-12-04 00:23:35 -0500

    [72] changed the infinite loop to run in background

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -21,6 +21,6 @@ docker run -d --name sgastormclustering -p 8083:8080 isgastormclustering
 echo "Changing the directory to SGA_Rest_StormClustering"
 cd /home/ec2-user/SGA_Rest_StormClustering
 
-nohup mvn exec:java -Dexec.mainClass=edu.sga.sangam.resources.StormClusteringMain &
+nohup mvn exec:java -Dexec.mainClass=edu.sga.sangam.resources.StormClusteringMain > /home/ec2-user/maven.out 2>&1 &
 
 docker rmi $(docker images -f "dangling=true" -q) || true
