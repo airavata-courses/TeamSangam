c9a466cc47b500d2cdaadffb8e768d9955e6b29a
praddyinstant <pradeep421@gmail.com>
2016-12-03 21:41:59 -0500

    corrected the script file to populate the IP address correctly [72]

--- env_scripts/props.sh
+++ env_scripts/props.sh
@@ -2,3 +2,4 @@
 
 echo 'SERVER_1:'$PRIVATE_IP1:$PUBLIC_IP1 > ../SGA_Rest_StormClustering/system.properties
 echo 'SERVER_2:'$PRIVATE_IP2:$PUBLIC_IP2 >> ../SGA_Rest_StormClustering/system.properties
+echo 'SERVER_3:'$GATEWAY_PRIVATE_IP:$GATEWAY_PUBLIC_IP >> ../SGA_Rest_StormClustering/system.properties
