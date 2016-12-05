0c99a134e0e42e7e20d0fa46ba74a313f2506054
ramarvab <ramarvab@gmail.com>
2016-12-05 02:30:12 -0500

    registry url updated

--- SGA_REST_WeatherForecastClient/src/main/java/edu/sga/sangam/services/WeatherClientOrchestrator.java
+++ SGA_REST_WeatherForecastClient/src/main/java/edu/sga/sangam/services/WeatherClientOrchestrator.java
@@ -123,10 +123,10 @@ public class WeatherClientOrchestrator {
 		DateFormat df2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 		request.put("time", df2.format(date));
 		HttpClient client = new HttpClient();
-		ZooKeeperClient service = new ZooKeeperClient();
-		String registryURL = service.discoverServiceURI("registry");
+		//ZooKeeperClient service = new ZooKeeperClient();
+		//String registryURL = service.discoverServiceURI("registry");
 		
-		PostMethod post = new PostMethod(registryURL+"/orchestrator");
+		PostMethod post = new PostMethod("http://54.193.9.14:8085/SGA_REST_Registry/sga/registry/orchestrator");
 		StringRequestEntity entity;
 		try {
 			entity = new StringRequestEntity(request.toJSONString(), "application/json", "UTF-8");
