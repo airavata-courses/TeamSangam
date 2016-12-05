dbfd6bf7c939a94955e66dcc9f1a44e875928018
ramarvab <ramarvab@gmail.com>
2016-12-05 02:13:39 -0500

    [#72] added log file

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -9,6 +9,11 @@ fi
 echo "Starting new docker container sgagateway"
 docker run -d --name sgagateway -p 8080:8080 isgagateway
 
+cd /home/ec2-user/SGA_REST_WeatherForecastClient/
+nohup mvn exec:java -Dexec.mainClass=edu.sga.sangam.services.ResultMain > /home/ec2-user/SGA_REST_WeatherForecastClient/result.out 2>&1 &
+
 echo "Removing all the dangling docker images"
 docker rmi $(docker images -f "dangling=true" -q) || true
+
+
 
