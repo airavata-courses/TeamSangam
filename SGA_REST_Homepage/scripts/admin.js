ed4a7c10c8f55ba08193d3523d5e0a50c9855c0c
Raja Simha <rajasimha1991@gmail.com>
2016-12-06 03:27:10 -0500

    [#45] updated admin module

--- SGA_REST_Homepage/scripts/admin.js
+++ SGA_REST_Homepage/scripts/admin.js
@@ -5,31 +5,64 @@ var admin = angular .module("sga_admin",[])
 						// There is no message to display at the beginning.
 						$scope.message = "";
 						// We show the table using the ng-show directive only when the button is clicked.
-						$scope.showTable = false;
-						$scope.pullAllUserStats = function() {
+						$scope.showUsers = false;
+						$scope.showUserDetails = false;
+						$scope.pullAllUsers = function() {
 							// An http request to fetch the json object with all user details. For now, hard coding the json data as below.
 							$http({
 									method : 'GET',
 									url : myurl + "8085/SGA_REST_Registry/sga/registry/getstats"
 							})
-							// var data = {
-							// 		"users":[
-							// 					{"id": "user1@yahoo.co.in", "count": 66},
-							// 					{"id": "user2@gmail.com", "count": 5}
-							// 				]
-							// 			};
-							// if the server was reached and valid response was received, then below.
+							/* var data = {
+							 		"users":[
+							 					{"_id": "user1@yahoo.co.in", "count": 66},
+							 					{"_id": "user2@gmail.com", "count": 5}
+							 				]
+							 			};
+							$scope.stats = data.users; */
+							 //if the server was reached and valid response was received, then below.
 							.then(function(response){
 								var data = response.data;
 								$scope.stats = data.users;
 								$scope.message = "Below are the counts of all the requests submitted by all the users.";
-								$scope.showTable = true;
+								$scope.showUsers = true;
+								$scope.showUserDetails = false;
 							},
 							// if the request was not successful
 							function(response){
 								$scope.message = "There was an error processing your request. Please try again after sometime.";
 							});
 						};
+						
+						$scope.pullUserStats = function(email) {
+							//console.log(email);
+							
+							/* var data = {
+							 		"users":[
+							 					{"sessionid": "whatever",  "orchestrator" : "request success"},
+							 					{"sessionid": "whatever2",  "orchestrator" : "request failure"}
+							 				]
+							 			};
+							$scope.stats = data.users; */
+							// An http request to get all session details of a user.
+							$http({
+									method : 'GET',
+									url : myurl + "8085/SGA_REST_Registry/sga/registry/getuserstats",
+									data : {"email" : email}
+							})
+							.then(function(response){
+								var data = response.data;
+								$scope.stats = data.users;
+								$scope.message = "Below are the details of "+email+".";
+								$scope.showUsers = false;
+								$scope.showUserDetails = true;
+							},
+							// if the request was not successful
+							function(response){
+								$scope.message = "There was an error processing your request. Please try again after sometime.";
+							});
+						};
+						
 						$scope.logout = function(){
 							$scope.message = "Logging you out...";
 							$http({
