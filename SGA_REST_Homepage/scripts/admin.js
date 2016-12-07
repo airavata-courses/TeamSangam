9c6851494565a047eec374d71e6b3560332bdfeb
Vikas Palakurthi <vikaspro90@gmail.com>
2016-12-07 00:08:20 -0500

    [#79]instance change

--- SGA_REST_Homepage/scripts/admin.js
+++ SGA_REST_Homepage/scripts/admin.js
@@ -1,4 +1,4 @@
-var myurl = "http://ec2-54-183-233-167.us-west-1.compute.amazonaws.com:";
+var myurl = "http://ec2-54-183-166-68.us-west-1.compute.amazonaws.com:";
 
 var admin = angular .module("sga_admin",[])
 					.controller("sga_admin_controller", function($scope, $http) {
