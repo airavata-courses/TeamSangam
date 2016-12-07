c336a182f24372040a5d94d57e88ac1103bf6e58
Vikas Palakurthi <vikaspro90@gmail.com>
2016-12-06 23:40:37 -0500

    [#79]instance change

--- SGA_REST_login/scripts/home.js
+++ SGA_REST_login/scripts/home.js
@@ -2,7 +2,7 @@ function validEmail(email) {
  	var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
  	return regex.test(email);
 }
-var instanceURL = "http://ec2-54-183-132-116.us-west-1.compute.amazonaws.com:5000/";
+var instanceURL = "http://ec2-54-183-233-167.us-west-1.compute.amazonaws.com:5000/";
 var login = angular .module("authModule",[])
 					.controller("loginController", function($scope, $http, $window, $sce){
 						$scope.page = "templates/normalLogin.html"
