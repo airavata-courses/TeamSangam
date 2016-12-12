b34fa6302b8e8aeb583cdbb3f84647b4f829ce9f
praddyinstant <pradeep421@gmail.com>
2016-12-11 19:10:43 -0500

    [79] deploy login page html & .js files into Gateway docker

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -32,5 +32,17 @@ fi
 echo "Starting new docker container sgalogin."
 docker run -d --link usermysqldb:usermysqldb --name sgalogin -p 5000:5000 isgalogin
 
+if [[ $(docker ps -a -f name=sgagateway -q) ]]; then
+	echo "Adding the login files to sgagateway docker container"
+	docker cp /home/ec2-user/SGA_REST_login/scripts sgagateway:/usr/local/tomcat/webapps/ROOT/
+	docker cp /home/ec2-user/SGA_REST_login/stylesheets sgagateway:/usr/local/tomcat/webapps/ROOT/
+	docker cp /home/ec2-user/SGA_REST_login/templates sgagateway:/usr/local/tomcat/webapps/ROOT/
+	docker cp /home/ec2-user/SGA_REST_login/login.html sgagateway:/usr/local/tomcat/webapps/ROOT/
+	docker cp /home/ec2-user/SGA_REST_login/signup.html sgagateway:/usr/local/tomcat/webapps/ROOT/
+	docker cp /home/ec2-user/SGA_REST_login/loginError.html sgagateway:/usr/local/tomcat/webapps/ROOT/
+	docker cp /home/ec2-user/SGA_REST_login/index.html sgagateway:/usr/local/tomcat/webapps/ROOT/
+fi
+
+
 echo "Removing dangling nodes, if any"
 docker rmi $(docker images -f "dangling=true" -q) || true
