8e2823b4552cb5ebe2c1300a12c10ac7992a111e
praddyinstant <pradeep421@gmail.com>
2016-12-11 19:35:03 -0500

    [79] corrected the sql container stopped status check

--- CodeDeployScripts/install.sh
+++ CodeDeployScripts/install.sh
@@ -1,12 +1,16 @@
 set -e
 
 #docker run -d --name mongo -p 27017:27017 imongo
-if [[ $(docker ps -a -f name=usermysqldb -q) ]]; then
+if [[ $(docker ps -f name=usermysqldb -q) ]]; then
 	echo "MySQL docker container is already running."
 else	
-	#docker rm -f usermysqldb || true
-	echo "Starting new docker container usermysqldb."
-	docker run -d --name usermysqldb -p 3306:3306 iusermysqldb
+	if [[ $(docker ps -a -f name=usermysqldb -q) ]]; then
+		docker rm -f usermysqldb || true
+		echo "Starting new docker container usermysqldb."
+		docker run -d --name usermysqldb -p 3306:3306 iusermysqldb
+	else
+		echo "Starting new docker container usermysqldb."
+		docker run -d --name usermysqldb -p 3306:3306 iusermysqldb
 fi
 
 #docker run -d --name usermysqldb -p 3306:3306 iusermysqldb
@@ -15,15 +19,6 @@ fi
 
 #docker run -d --name sgagateway -p 8080:8080 isgagateway
 
-#docker run -d --link mongo:mongo --name sgadataingest -p 8081:8080 isgadataingest
-
-#docker run -d --name sgastormdetection -p 8082:8080 isgastormdetection
-
-#docker run -d --name sgastormclustering -p 8083:8080 isgastormclustering
-
-#docker run -d --name sgaforecast -p 8084:8080 isgaforecast
-
-#docker run -d --link mongo:mongo --name sgaregistry -p 8085:8080 isgaregistry
 
 if [[ $(docker ps -a -f name=sgalogin -q) ]]; then
 	echo "Removing the existing sgalogin docker container."
