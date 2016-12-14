8c0ef5ada450920772f23ca884ca8b6527c7f5b2
Vikas Palakurthi <vikaspro90@gmail.com>
2016-12-14 17:57:49 -0500

    [#91]

--- SGA_REST_Homepage/scripts/main.js
+++ SGA_REST_Homepage/scripts/main.js
@@ -193,7 +193,7 @@ home.controller("sga_controller", function ($scope, $http, $window, $interval) {
 				$scope.jobs.push(job);
 			}
 			$scope.jobs.reverse();
-
+			
 			if($scope.jobs){
 				$scope.tableMessage = "Below are all the jobs you have ever submitted. Click on the resubmit to get a job submission form with the corresponding parameters. You can edit the parameters as desired.";
 			} else {
