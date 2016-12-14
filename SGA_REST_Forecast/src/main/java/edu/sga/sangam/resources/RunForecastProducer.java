57c133200c3efea87d0fe810dbd88105433d111d
ramarvab <ramarvab@gmail.com>
2016-12-14 03:23:42 -0500

    changed ip address of registry

--- SGA_REST_Forecast/src/main/java/edu/sga/sangam/resources/RunForecastProducer.java
+++ SGA_REST_Forecast/src/main/java/edu/sga/sangam/resources/RunForecastProducer.java
@@ -97,9 +97,9 @@ public class RunForecastProducer implements Runnable {
 		requestDataIngestor.put("rftime", df2.format(date));
 		HttpClient client = new HttpClient();
 		logger.info("logging run forecast details");
-		//ZooKeeperService services =  new ZooKeeperService();
-		//String registryURL = services.discoverServiceURI("registry");
-		PostMethod post = new PostMethod("http://54.67.29.184:8085/SGA_REST_Registry/sga/registry/runforecast");
+		ZooKeeperService services =  new ZooKeeperService();
+		String registryURL = services.discoverServiceURI("registry");
+		PostMethod post = new PostMethod(registryURL+"/runforecast");
 		StringRequestEntity entity;
 		try {
 			entity = new StringRequestEntity(requestDataIngestor.toJSONString(), "application/json", "UTF-8");
