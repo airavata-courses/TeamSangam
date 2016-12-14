64da7ca781c9b0a3dd8cb65dfa2ba7eb0efa5692
Raja Simha <rajasimha1991@gmail.com>
2016-12-13 22:58:14 -0500

    [#92] updated deployment scripts

--- CodeDeployScripts/afterInstall.sh
+++ CodeDeployScripts/afterInstall.sh
@@ -17,20 +17,20 @@ gateway='GATEWAY'
 
 if [ "$serverID" == "$gateway" ]; then
         mongo --eval "rs.initiate()"
-	while [ $(curl -s -o /dev/null --connect-timeout 10 -w "%{http_code}" http://54.183.160.22:27017/) -ne 200 ]
+	while [ $(curl -s -o /dev/null --connect-timeout 10 -w "%{http_code}" http://54.67.29.184:27017/) -ne 200 ]
 	do
 		sleep 20
 	done
-		mongo --eval 'rs.add("54.183.160.22")'
+		mongo --eval 'rs.add("54.67.29.184")'
 	
-	while [ $(curl -s -o /dev/null --connect-timeout 10 -w "%{http_code}" http://54.193.102.104:27017/) -ne 200 ]
+	while [ $(curl -s -o /dev/null --connect-timeout 10 -w "%{http_code}" http://54.215.209.204:27017/) -ne 200 ]
         do
                 sleep 20
         done
-                mongo --eval 'rs.add("54.193.102.104")'
+                mongo --eval 'rs.add("54.215.209.204")'
 
 else
-	while [ $(curl -s -o /dev/null --connect-timeout 10 -w "%{http_code}" http://54.183.233.167:27017/) -ne 200 ]
+	while [ $(curl -s -o /dev/null --connect-timeout 10 -w "%{http_code}" http://54.193.116.150:27017/) -ne 200 ]
         do
                 sleep 120
         done
