1feeee254bb920f61ce65eea8dec1c6c00dde18b
ramarvab <ramarvab@gmail.com>
2016-12-14 02:16:20 -0500

    modified the result output #91

---
+++ SGA_REST_Registry/src/main/java/edu/sga/sangam/resources/Result.java
@@ -0,0 +1,13 @@
+package edu.sga.sangam.resources;
+
+public class Result{
+	String result;
+
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+}
