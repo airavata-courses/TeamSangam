af98118c8a950cd1cba8a72e3b8df34e9c371307
praddyinstant <pradeep421@gmail.com>
2016-12-15 16:13:05 -0500

    changing the IP references to worker1

--- SGA_REST_Registry/src/main/java/edu/sga/sangam/db/DBConstants.java
+++ SGA_REST_Registry/src/main/java/edu/sga/sangam/db/DBConstants.java
@@ -3,7 +3,7 @@ package edu.sga.sangam.db;
 public class DBConstants {
 	static public final String DB_HOST ="localhost";
 	static public final String gateway = "54.193.113.156";
-	static public final String worker1 = "54.183.166.68";
+	static public final String worker1 = "54.67.55.68";
 	static public final String worker2 = "54.183.167.119";
 	static public final int DB_Port =27017;
 	static public final String DB_Name ="db_SGA";
