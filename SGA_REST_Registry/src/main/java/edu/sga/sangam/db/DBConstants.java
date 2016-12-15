4ffc229784127595455f3d03df0c2f263fe6dc8f
Raja Simha <rajasimha1991@gmail.com>
2016-12-15 18:29:00 -0500

    updated mongo replica set ips

--- SGA_REST_Registry/src/main/java/edu/sga/sangam/db/DBConstants.java
+++ SGA_REST_Registry/src/main/java/edu/sga/sangam/db/DBConstants.java
@@ -3,8 +3,8 @@ package edu.sga.sangam.db;
 public class DBConstants {
 	static public final String DB_HOST ="localhost";
 	static public final String gateway = "54.193.116.150";
-	static public final String worker1 = "54.67.29.184";
-	static public final String worker2 = "54.215.209.204";
+	static public final String worker1 = "54.67.40.162";
+	static public final String worker2 = "52.53.201.39";
 	static public final int DB_Port =27017;
 	static public final String DB_Name ="db_SGA";
 	static public final String DB_Collection_Log ="registry_collection";
